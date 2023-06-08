6fb61413a13654182e8f4d29ce98710a94c22ee8
windofme1109 <wuzengyan1314@163.com>
2023-06-08 22:20:50 +0800

    Rust 中的异步编程 - 7

--- async-multi-executor/src/main.rs
+++ async-multi-executor/src/main.rs
@@ -208,18 +208,24 @@ async fn run_loop(
     loop {
         select! {
             () = interval_timer.select_next_some() => {
+                // // 定时器已结束，若`get_new_num_fut`没有在运行，就创建一个新的
                 if get_new_num_fut.is_terminated() {
                     get_new_num_fut.set(get_new_num().fuse())
                 }
             },
             new_num = get_new_num_fut => {
+                // 收到新的数字 -- 创建一个新的`run_on_new_num_fut`并丢弃掉旧的
                 run_on_new_num_fut.set(run_on_new_num().fuse())
             },
+            // 运行 `run_on_new_num_fut`
             () = run_on_new_num_fut => {},
+            // 若所有任务都完成，直接 `panic`， 原因是 `interval_timer` 应该连续不断的产生值，而不是结束
+            // 后，执行到 `complete` 分支
             complete => panic!("`interval_timer` completed unexpectedly")
         }
     }
 }
+
 
 
 use futures::future;

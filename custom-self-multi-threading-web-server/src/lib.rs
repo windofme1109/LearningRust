b446e0c2e0fbca4a11b713f9d395a77e89d63b78
windofme1109 <wuzengyan1314@163.com>
2023-08-23 20:24:55 +0800

    多线程 HTTP 服务器 - 4 - 停机与清理 - 1

--- custom-self-multi-threading-web-server/src/lib.rs
+++ custom-self-multi-threading-web-server/src/lib.rs
@@ -2,11 +2,19 @@
 use std::thread;
 use std::sync::{Arc, mpsc, Mutex};
 
+// 定义这个枚举的作用是：
+// Message 作为在通道中发送的消息的主体，用来取代 Job
+// 在线程中，根据 Message 的不同变体，从而执行不同的操作
+enum Message {
+    NewJob(Job),
+    Terminated
+}
+
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
     // 通道的发送端，发送的类型是 Job
-    sender: mpsc::Sender<Job>
+    sender: mpsc::Sender<Message>
 }
 
 type Job = Box< dyn FnOnce() + Send + 'static>;
@@ -19,22 +27,43 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         println!("id {}", id);
         let thread = thread::spawn(move || {
             loop {
-                //
+                // 下面这种先获取锁，再从锁中获取信息（通过 recv 方法实现）
+                // 存在一个问题：就是所有的请求，只会由第一个线程来处理
+                // 原因可能是：我们首先获取了锁：rx，然后不断轮询，就会导致第一个线程不断的持有锁
+                // 进而从锁中获取收到的闭包，然后执行
+
+                // 而实际上，整体的过程应该是：拿到锁 -> 从锁中获取信息 -> 释放锁
+                // 这样其他线程才有机会拿到锁
+
                 // let rx = receiver.lock().unwrap();
                 // 拿到 job
                 // let job = rx.recv().unwrap();
 
-                // 尝试获取锁
-                let job = receiver.lock().unwrap().recv().unwrap();
+                // 尝试获取锁，下面这种链式调用，既可以实现上述的 “拿到锁 -> 从锁中获取信息 -> 释放锁” 的过程
+                // let job = receiver.lock().unwrap().recv().unwrap();
 
-                println!("id {} thread received a job, executing", id);
+                // println!("id {} thread received a job, executing", id);
 
-                // 在线程中执行闭包
-                job();
+                // // 在线程中执行闭包
+                // job();
+
+                let msg = receiver.lock().unwrap().recv().unwrap();
+
+                match msg {
+                    Message::NewJob(job) => {
+                        // 接收到闭包，开始执行
+                        job();
+                    },
+                    Message::Terminated => {
+                        // 收到终止信号，就跳出循环
+                        break;
+                    }
+                }
+
             }
         });
 
@@ -54,8 +83,8 @@ impl ThreadPool {
         
         let mut workers = Vec::with_capacity(count);
         println!("count: {}", count);
+        // 通道的接收端和发送端持有的类型必须相同
         let (sender, receiver) = mpsc::channel();
-
         let receiver = Arc::new(Mutex::new(receiver));
 
         for id in 0..count {
@@ -67,7 +96,7 @@ impl ThreadPool {
         }
 
         ThreadPool {
-            workers: workers,
+            workers,
             sender
         }
 
@@ -85,8 +114,18 @@ impl ThreadPool {
     {
         // 使用通道将闭包 f 发送到线程上，因此这里需要使用智能指针进行包裹
         // 使用 Box 类型包裹 f
-        let executeFn = Box::new(f);
+        let job = Box::new(f);
 
-        self.sender.send(executeFn).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+// 为 ThreadPool 实现 Drop trait
+// 当 ThreadPool 实例离开作用域时，会执行一些操作
+// 我们这里执行的是清理线程的操作
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+        // 先发送一遍终止信号
     }
 }
